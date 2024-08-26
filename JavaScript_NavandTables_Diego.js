3324a5c90478ef5b7a5f0d6efc0385466d60f3b4
Diego Morales Villafana <130813807+Owldatzu@users.noreply.github.com>
2024-08-26 01:34:03 -0500

    Add files via upload

--- JavaScript_NavandTables_Diego.js
+++ JavaScript_NavandTables_Diego.js
@@ -1,10 +1,10 @@
 
-
+// all these functions serve the same purpose of translating the user to another page
 function UserLogin()
 {
     location.replace("Assignment_3.1_PracticeUserInput_Diego.html");
 }
-
+// this function will open a new tab where the user will be transported to
 function fightGame()
 {
     window.open("Assignment_2.1_Sounds_FightGame2_Diego.html");
@@ -35,7 +35,7 @@ function homePage()
     location.replace("Assignment_5.1_NavAndTables_Diego.html");
 }
 
-
+// this function will reset the current page completely 
 function reload()
 {
     location.reload();
