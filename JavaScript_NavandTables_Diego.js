627684cce306ca4aece4b432b46d2f511e908ca0
Diego Morales Villafana <130813807+Owldatzu@users.noreply.github.com>
2024-08-26 01:05:20 -0500

    Add files via upload

---
+++ JavaScript_NavandTables_Diego.js
@@ -0,0 +1,42 @@
+
+
+function UserLogin()
+{
+    location.replace("Assignment_3.1_PracticeUserInput_Diego.html");
+}
+
+function fightGame()
+{
+    window.open("Assignment_2.1_Sounds_FightGame2_Diego.html");
+}
+
+function boards()
+{
+    location.replace("Assignment_1.1_DiegoBoards_SecondPage.html");
+}
+
+function memePage()
+{
+    location.replace("Assignment_1.1_DiegoMemes.html");
+}
+
+function stringsPage()
+{
+    location.replace("Assignment_3.1_PracticeStrings_Diego.html");
+}
+
+function tablesPage()
+{
+    location.replace("Assignment_5.1_PracticeTables_Diego.html");
+}
+
+function homePage()
+{
+    location.replace("Assignment_5.1_NavAndTables_Diego.html");
+}
+
+
+function reload()
+{
+    location.reload();
+}
